2f55f19407f14c4bf068f46e5bd952fcdbf5dec2
restuarachman <restuarachman@gmail.com>
2022-12-29 09:21:43 +0700

    add lilbraries crs

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'upload', 'session', 'encrypt', 'image_lib', 'mainapi');
+$autoload['libraries'] = array('database', 'upload', 'session', 'encrypt', 'image_lib', 'mainapi', 'crs');
 
 
 /*
